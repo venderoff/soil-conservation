1a8967fa94b57b7e15e3e498d63dbdecc8615389
unknown <venderoff@gmail.com>
2024-03-24 03:38:25 +0530

    adding pages

---
+++ app/home/page.js
@@ -0,0 +1,18 @@
+export default function home(){
+
+    return(
+        <main>
+        <h1>Soil Conservation</h1>
+        <p> Many Examples can be given for soil conservation</p>
+        <ol>
+         <li>   Water Conservation </li>
+
+         
+         <li>Land Fill</li>
+        </ol>
+        </main>
+        
+    );
+
+
+}
