ba8166dff464e07eaa4644db196416ecf54024e0
unknown <venderoff@gmail.com>
2024-03-25 03:25:55 +0530

    pages organised

--- app/components/main-header.js
+++ app/components/main-header.js
@@ -1,14 +1,15 @@
 import LogoImage from '@/assets/header.jpg'
 import Link from 'next/link'; 
+import classes from './main-header.module.css'
 export default function MainHeader() {
 
     return(
-       <header>
-          <Link href="/" >
+       <header className={classes.header}>
+          <Link className={classes.logo}href="/" >
           <img src={LogoImage.src} alt="soil conservation" style={{ width: '90%', height: '100 px' }} />
           Soil Conservation
           </Link>
-        <nav>
+        <nav className={classes.nav}>
            <ul>
                <li><Link href="/about">About</Link></li>
                <li><Link href="/login">Login</Link></li>
--- app/page.js
+++ app/page.js
@@ -1,12 +1,49 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import classes from './page.module.css'
+
 import Link from "next/link";
+
 export default function Home() {
   return (
-    <main>
-      <h1>HomePage for Soil and Conservation</h1>
-      <p><Link href="/about">About</Link></p>
-      <p><Link href="/login">Login</Link></p>
+    <main className={classes.main}>
+      <h1 className={classes.p}>HomePage for Soil and Conservation</h1>
+      <div className={classes.description}>
+      <h3>Importance of Soil Conservation:</h3>      
+      <p className={classes.description}>Soil conservation is the practice of protecting the soil from erosion and degradation to ensure its sustainable use for agriculture, forestry, construction, and other purposes. It involves implementing various techniques and strategies to prevent soil erosion, maintain soil fertility, and preserve the overall health of the soil ecosystem. Soil conservation is crucial for ensuring food security, preserving biodiversity, and mitigating the negative impacts of climate change.
+
+ 
+<br/>
+1.  Preservation of Soil Fertility : Soil is a finite and valuable resource essential for agriculture and food production. Soil conservation practices help to maintain soil fertility by preventing the loss of organic matter, nutrients, and essential microorganisms.
+<br/>
+2.  Prevention of Soil Erosion : Soil erosion, caused by wind, water, or human activities, leads to the loss of topsoil and reduces soil productivity. Soil conservation measures such as terracing, contour plowing, and cover cropping help to reduce erosion and preserve soil structure.
+<br/>
+3.  Protection of Water Resources : Healthy soil plays a critical role in regulating the water cycle and maintaining water quality. Soil conservation practices help to prevent sedimentation and pollution of water bodies, thus safeguarding water resources for human consumption and ecosystem health.
+<br/>
+4.  Enhancement of Crop Yields : By improving soil structure, fertility, and water retention capacity, soil conservation practices contribute to increased crop yields and agricultural productivity. This is essential for meeting the growing demand for food in a sustainable manner.
+
+5.  Mitigation of Climate Change : Healthy soils act as carbon sinks, sequestering carbon dioxide from the atmosphere and helping to mitigate climate change. Soil conservation practices such as agroforestry, conservation tillage, and organic farming promote carbon sequestration and reduce greenhouse gas emissions.
+
+ Soil Conservation Techniques:
+
+1.  Contour Plowing : Plowing along the contour lines of the land reduces water runoff and soil erosion by slowing down the flow of water across the slope.
+
+2.  Terracing : Terracing involves constructing step-like structures on sloping land to create flat areas that reduce water runoff and soil erosion.
+
+3.  Cover Cropping : Planting cover crops such as legumes, grasses, or mulch helps to protect the soil from erosion, suppress weeds, and improve soil fertility.
+
+4.  Conservation Tillage : Conservation tillage practices minimize soil disturbance and maintain crop residues on the soil surface, reducing erosion and improving soil structure.
+
+5.  Windbreaks : Planting trees, shrubs, or other vegetation along the edges of fields helps to reduce wind erosion by creating a barrier that slows down the wind.
+
+6.  Crop Rotation : Rotating crops in a sequence helps to break pest cycles, improve soil health, and maintain soil fertility by alternating nutrient demands and reducing soil-borne diseases.
+
+Conclusion:
+
+Soil conservation is essential for sustaining agricultural productivity, protecting natural resources, and ensuring the long-term viability of ecosystems. By implementing effective soil conservation practices, we can safeguard the health of the soil, mitigate environmental degradation, and promote sustainable land use for future generations. It requires collaboration among farmers, policymakers, researchers, and communities to address the complex challenges of soil conservation and achieve sustainable development goals.</p>
+
+
+      </div>
     </main>
   );
 }
